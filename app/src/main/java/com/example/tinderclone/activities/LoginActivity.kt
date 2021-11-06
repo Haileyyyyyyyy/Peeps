ecb50f02dd7e95cea480e15ba0354215398fe2f5
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2021-11-06 20:16:11 +0900

    zz

--- app/src/main/java/com/example/tinderclone/activities/LoginActivity.kt
+++ app/src/main/java/com/example/tinderclone/activities/LoginActivity.kt
@@ -5,16 +5,45 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.tinderclone.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private var firebaseAuth = FirebaseAuth.getInstance()
+    private var firebaseAuthListener = FirebaseAuth.AuthStateListener{
+        val user = firebaseAuth.currentUser
+        if(user != null){
+            startActivity(MainActivity.newIntent(this))
+            finish()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
 
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth.addAuthStateListener(firebaseAuthListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        firebaseAuth.addAuthStateListener (firebaseAuthListener)
+    }
+
     fun onLogin(v: View){
-        startActivity(MainActivity.newIntent(this))
+        if(!emailET.text.toString().isNullOrEmpty() && !password_ET.text.toString().isNullOrEmpty()){
+            firebaseAuth.signInWithEmailAndPassword(emailET.text.toString(), password_ET.text.toString())
+                .addOnCompleteListener{task ->
+                    if(!task.isSuccessful){
+                        Toast.makeText(this,"Login error ${task.exception?.localizedMessage}",Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
     }
 
     companion object{
--- app/src/main/java/com/example/tinderclone/activities/SignupActivity.kt
+++ app/src/main/java/com/example/tinderclone/activities/SignupActivity.kt
@@ -5,15 +5,47 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.tinderclone.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
+
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private val firebaseAuthListener = FirebaseAuth.AuthStateListener{
+        val user = firebaseAuth.currentUser
+        if(user != null) {
+            startActivity(MainActivity.newIntent(this))
+            finish()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
     }
+
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth.addAuthStateListener (firebaseAuthListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener)
+    }
+
+
     fun onSignup(v: View){
-        startActivity(MainActivity.newIntent(this))
+        if(!emailET.text.toString().isNullOrEmpty() && !password_ET.text.toString().isNullOrEmpty()){
+            firebaseAuth.createUserWithEmailAndPassword(emailET.text.toString(), password_ET.text.toString())
+                .addOnCompleteListener{task ->
+                    if(!task.isSuccessful){
+                        Toast.makeText(this,"Signup error ${task.exception?.localizedMessage}",Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+        }
     }
 
     companion object{
